f328c34e27fabe76b74401d4598f355a74e8d0b0
Assinnata <matteo.github@assinnata.com>
2015-06-30 17:21:10 +0200

    api v2

--- scripts/master.py
+++ scripts/master.py
@@ -4,9 +4,13 @@ import json
 
 application = Flask(__name__)
 
-@application.route("/deerwallet", methods=["POST"])
+@application.route("/deerwallet/v2", methods=["POST"])
 def deerwalletpost():
 	return jsonify(deersrv.proc(json.loads(request.data)))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify(deersrv.Error('INVALID_REQUEST_TYPE').get()), 404
 
 if __name__ == "__main__":
     application.run(host='127.0.0.1')
